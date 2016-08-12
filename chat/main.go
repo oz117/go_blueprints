f9be06a3c25c6c21fb86a7cd6e4cf82b23c7569f
Andre PAULOS <andre.paulos@etixgroup.com>
2016-08-12 19:22:59 +0200

    #comment Server address can now passed via the command line

--- chat/main.go
+++ chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -21,18 +22,21 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("../templates",
 			t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "Address of the application")
+	flag.Parse()
 	r := client.NewRoom()
 	// Handle request arriving on /
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("../templates/js"))))
 	http.Handle("/room", r)
 	go r.Run()
+	log.Printf("Starting web server on [%s]", *addr)
 	// Start a webserver on 8080
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal(err)
 	}
 }
