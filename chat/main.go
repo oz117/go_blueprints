4887504a18e8845e3d7f64fd389d45b71826bacc
Andre PAULOS <andre.paulos@etixgroup.com>
2016-08-12 16:48:58 +0200

    #comment changed to use the client package

--- chat/main.go
+++ chat/main.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"path/filepath"
 	"sync"
+
+	"github.com/go_blueprints/chat/client"
 )
 
 type templateHandler struct {
@@ -16,15 +18,19 @@ type templateHandler struct {
 
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
-		t.templ = template.Must(template.ParseFiles(filepath.Join("templates",
+		t.templ = template.Must(template.ParseFiles(filepath.Join("../templates",
 			t.filename)))
 	})
 	t.templ.Execute(w, nil)
 }
 
 func main() {
+	r := client.NewRoom()
 	// Handle request arriving on /
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("../templates/js"))))
+	http.Handle("/room", r)
+	go r.Run()
 	// Start a webserver on 8080
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
