71d9022577cc88a8216366e19c3c4ee2ea74cd2c
Andre PAULOS <andre.paulos@etixgroup.com>
2016-08-12 09:20:19 +0200

    #comment Addition of a templateHandler to make things prettier

--- chat/main.go
+++ chat/main.go
@@ -1,24 +1,30 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates",
+			t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
 
 func main() {
 	// Handle request arriving on /
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-      <html>
-        <head>
-          <title>Chat</title>
-        </head>
-        <body>
-          Let's ... play a game
-        </body>
-      </html>
-      `))
-	})
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 	// Start a webserver on 8080
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
