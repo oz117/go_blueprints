b3728c024ecf3bf2724122d361f6af8861469ec2
Andre Paulos <andre.paulos@epitech.eu>
2016-08-16 11:33:46 +0200

    #comment created subdirectory for chapter 2

---
+++ chapter_2/chat/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	"github.com/go_blueprints/chat/client"
+	"github.com/go_blueprints/chat/trace"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("../templates",
+			t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "Address of the application")
+	var verbose = flag.String("V", "false", "Display more information about what's going on")
+	flag.Parse()
+	r := client.NewRoom()
+	if strings.Compare(*verbose, "true") == 0 {
+		r.Tracer = trace.New(os.Stdout)
+	}
+	// Handle request arriving on /
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("../templates/js"))))
+	http.Handle("/room", r)
+	go r.Run()
+	log.Printf("Starting web server on [%s]", *addr)
+	// Start a webserver on 8080
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal(err)
+	}
+}
