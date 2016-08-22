6718e8d829f19f23f353f4419c418cfe5835ff28
Andre PAULOS <andre.paulos@etixgroup.com>
2016-08-22 17:11:08 +0200

    #comment added callback action

--- chapter_2/chat/auth/auth.go
+++ chapter_2/chat/auth/auth.go
@@ -53,6 +53,29 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		w.Header().Set("Location", loginURL)
 		w.WriteHeader(http.StatusTemporaryRedirect)
+	case "callback":
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			log.Fatalln("Error when trying to get provider", provider, err)
+		}
+		creds, err := provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
+		if err != nil {
+			log.Fatalln("Error when trying to complte auth for", provider, "-", err)
+		}
+		user, err := provider.GetUser(creds)
+		if err != nil {
+			log.Fatalln("Error when trying to get user from", provider, "-", err)
+		}
+		authCookieValue := objx.New(map[string]interface{}{
+			"name": user.Name(),
+		}).MustBase64()
+		http.SetCookie(w, &http.Cookie{
+			Name:  "auth",
+			Value: authCookieValue,
+			Path:  "/",
+		})
+		w.Header().Set("Location", "/chat")
+		w.WriteHeader(http.StatusTemporaryRedirect)
 	default:
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprintf(w, "Auth action [%s] not supported", action)
