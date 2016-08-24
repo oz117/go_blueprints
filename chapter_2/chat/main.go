b73e36664e68a38dcfa6e6846f4a4666a46a845f
Andre Paulos <andre.paulos@epitech.eu>
2016-08-24 10:18:13 +0200

    #comment using new object to read config files

--- chapter_2/chat/main.go
+++ chapter_2/chat/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"html/template"
 	"log"
@@ -11,9 +10,10 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/go_blueprints/chapter_1/chat/client"
+	"github.com/go_blueprints/chapter_1/chat/trace"
 	"github.com/go_blueprints/chapter_2/chat/auth"
-	"github.com/go_blueprints/chapter_2/chat/client"
-	"github.com/go_blueprints/chapter_2/chat/trace"
+	"github.com/go_blueprints/chapter_2/chat/utils"
 	"github.com/stretchr/gomniauth"
 	"github.com/stretchr/gomniauth/providers/google"
 	"github.com/stretchr/objx"
@@ -24,12 +24,6 @@ type templateHandler struct {
 	once     sync.Once
 	filename string
 	templ    *template.Template
-}
-
-type provider struct {
-	Id       string `json:"id"`
-	Secret   string `json:"secret"`
-	Callback string `json:"callback"`
 }
 
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -46,37 +40,26 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.templ.Execute(w, data)
 }
 
-// This function searches for the configuration in /tmp/ and sets the params
-// for the provider
-func getProviderConfig() (p *provider, err error) {
-	file, err := os.Open("/tmp/config.json")
-	if err != nil {
-		return
-	}
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&p)
-	if err != nil {
-		return
-	}
-	return
-}
-
 func main() {
 	var addr = flag.String("addr", ":8080", "Address of the application")
 	var verbose = flag.String("V", "false", "Display more information about what's going on")
+
 	flag.Parse()
-	p, err := getProviderConfig()
+	config := utils.NewConfiguration()
+	r := client.NewRoom()
+	if strings.Compare(*verbose, "true") == 0 {
+		r.Tracer = trace.New(os.Stdout)
+		config.Tracer = trace.New(os.Stdout)
+	}
+	providers, err := config.GetProviderConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
 	gomniauth.SetSecurityKey(signature.RandomKey(64))
 	gomniauth.WithProviders(
-		google.New(p.Id, p.Secret, p.Callback),
+		google.New((*providers)[0].AppID, (*providers)[0].Secret,
+			(*providers)[0].Callback),
 	)
-	r := client.NewRoom()
-	if strings.Compare(*verbose, "true") == 0 {
-		r.Tracer = trace.New(os.Stdout)
-	}
 	// Handle request arriving on /
 	http.Handle("/", auth.MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
