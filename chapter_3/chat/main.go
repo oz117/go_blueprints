343dc9e30912a1dda3718465b7b5a0e753e733db
Andre Paulos <andre.paulos@epitech.eu>
2016-09-09 13:29:17 +0200

    Using gravatar as the source for the avatar

--- chapter_3/chat/main.go
+++ chapter_3/chat/main.go
@@ -46,7 +46,7 @@ func main() {
 
 	flag.Parse()
 	config := utils.NewConfiguration()
-	r := client.NewRoom(client.UseAuthAvatar)
+	r := client.NewRoom(client.UseGravatarAvatar)
 	if strings.Compare(*verbose, "true") == 0 {
 		r.Tracer = trace.New(os.Stdout)
 		config.Tracer = trace.New(os.Stdout)
